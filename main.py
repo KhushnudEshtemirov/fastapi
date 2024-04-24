c81c705722daf671fcaafee9e581f1335f4dff79
Khushnud Eshtemirov <khushnudeshtemirov@gmail.com>
2024-04-24 16:13:41 +0500

    Updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -30,9 +31,9 @@ async def get_current_user():
     return {"message": "This is current user."}
 
 
-# @app.get("/users/{user_id}")
-# async def user_id(user_id: str):
-#     return {"user_id": user_id}
+@app.get("/users/{user_id}")
+async def user_id(user_id: str):
+    return {"user_id": user_id}
 
 
 class FoodEnum(str, Enum):
@@ -50,3 +51,56 @@ async def get_food(food_name: FoodEnum):
         return {"food_name": food_name, "message": "You're still healthy"}
 
     return {"food_name": food_name, "message": "You're not so healthy bro. Come on!!!"}
+
+
+fake_item_db = [
+    {"item_name": "Car"},
+    {"item_name": "Bus"},
+    {"item_name": "Train"},
+    {"item_name": "Plane"},
+]
+
+
+@app.get("/vehicles")
+async def list_vehicles(skip: int = 0, limit: int = 10):
+    return fake_item_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_item(
+    item_id: str, sample_query_param: str, q: str | None = None, short: bool = True
+):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
+    if q:
+        item.update({"query": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quam."
+            }
+        )
+    return item
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.get("/items")
+async def read_items(q: str | None = Query(None, min_length=3, max_length=10)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
